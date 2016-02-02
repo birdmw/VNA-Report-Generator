eca2603d8c7cddcebcd88baeacb12770fa4fab22
Matthew Bird <birdmw@gmail.com>
2016-02-01 18:11:10 -0800

    Various bug fixes - and made plot parameters visible

--- report_generator.py
+++ report_generator.py
@@ -14,18 +14,17 @@ import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
-import seaborn as sns
 from docx.shared import Inches
 
 
 class Generator(object):
-    '''Generator Class - This object combines several files (info.csv, template.docx, guide.csv, and many .s4p files)'''
+    '''Generator Class - This object combines several files (info.csv, template.docx, guide.csv, and many .sNp files)'''
 
     def __init__(self):
         self.template_path = None
         self.guide_path = None
         self.info_path = None
-        self.s4p_dir = None
+        self.sNp_dir = None
         self.figure_string = None
         self.figures = None
 
@@ -33,10 +32,10 @@ class Generator(object):
         self.d_info = {}
         self.tpl = None
         self.df_guide = pd.DataFrame()
-        self.s4p_paths = None
-        self.s4p = []
+        self.sNp_paths = None
+        self.sNp = []
 
-        self.ts_manager = None #self.ts_manager.db[0][1][0] ====> DB, TSfile 0, S21
+        self.ts_manager = None  # self.ts_manager.db[0][1][0] ====> DB, TSfile 0, S21
         self.plots = None
         self.stop_frequency = None
         self.start_frequency = None
@@ -46,7 +45,7 @@ class Generator(object):
         self.read_guide()
         self.read_template()
         self.read_info()
-        self.read_s4ps()
+        self.read_sNps()
 
     def read_template(self):
         file_path = self.template_path + "\\template.docx"
@@ -71,36 +70,39 @@ class Generator(object):
                 self.df_guide = pd.read_csv(file_path)
                 self.set_ts_port_maps()
 
-
     def generate_guide(self):
-        self.get_s4p_paths()
+        self.get_sNp_paths()
         figures = self.get_guides()
         col_names = ['', 'title', 'magphase']
-        for i_p in range(len(self.s4p_paths)):
-            col_names.append('s4p_'+str(i_p))
+        for i_p in range(len(self.sNp_paths)):
+            col_names.append('sNp_'+str(i_p))
         self.df_guide = pd.DataFrame(columns=col_names) #put in column names
         s = ['paths', '', '']
-        for p in self.s4p_paths:
+        for p in self.sNp_paths:
             s.append(p)
         df_s = pd.DataFrame([s], columns=col_names) # put in row 1 with paths
         self.df_guide=self.df_guide.append(df_s)
         port_map = ['portmap', '', '']
-        for _ in self.s4p_paths:
+        for _ in self.sNp_paths:
             port_map.append('13_24')
         df_portmap = pd.DataFrame([port_map], columns=col_names)
         self.df_guide = self.df_guide.append(df_portmap)
         for i_pl in range(len(figures)):
             s = ["plot_"+str(i_pl), "plot_"+str(i_pl), self.figures[i_pl][0]]
-            for _ in self.s4p_paths:
+            for _ in self.sNp_paths:
                 #print self.figures[i_pl]
                 s.append("s"+self.figures[i_pl][1]+"_"+self.figures[i_pl][2])
             self.df_guide = self.df_guide.append(pd.DataFrame([s], columns=col_names))
         self.df_guide = self.df_guide.set_index(self.df_guide.columns[0])
         return self.df_guide
 
-    def get_s4p_paths(self):
-        self.s4p_paths = glob.glob(self.s4p_dir+"/*.s4p")
-        return self.s4p_paths
+    def get_sNp_paths(self):
+        if os.path.isfile(self.info_path + "\\guide.csv"):
+            self.sNp_paths = self.df_guide.iloc[0,3:].values.tolist()
+            print self.df_guide.iloc[0,3:].values.tolist()
+        else:
+            self.sNp_paths = glob.glob(self.sNp_dir+"/*.s*p")
+        return self.sNp_paths
 
     def get_guides(self):
         split_figures = self.figure_string.split(",")
@@ -146,10 +148,10 @@ class Generator(object):
             else:
                 self.ts_port_maps[p] = 1
 
-    def read_s4ps(self):
-        self.s4p_paths = self.get_s4p_paths()
+    def read_sNps(self):
+        self.sNp_paths = self.get_sNp_paths()
         self.set_ts_port_maps()
-        self.ts_manager = TouchstoneManager(self.s4p_paths, self.ts_port_maps)
+        self.ts_manager = TouchstoneManager(self.sNp_paths, self.ts_port_maps)
 
     def write_from_data(self):
         self.gen_plots()
@@ -162,7 +164,6 @@ class Generator(object):
 
             if not(self.df_info is None):
                 index = self.df_info['key'].tolist()
-                print "HERE====>"
                 dut_start_index = index.index('<DUT>')
                 dut_end_index = index.index('</DUT>')
                 port_start_index = index.index('<PORT>')
@@ -174,6 +175,7 @@ class Generator(object):
             sd1 = self.tpl.new_subdoc()
             sd2 = self.tpl.new_subdoc()
             sd3 = self.tpl.new_subdoc()
+            sd4 = self.tpl.new_subdoc()
 
             table1 = sd1.add_table(rows=len(dut_table), cols=len(dut_table[0]))
             table1.style = 'TableGrid'
@@ -187,12 +189,22 @@ class Generator(object):
                 for col in range(len(port_table[0])):
                     table2.cell(row, col).text = port_table[row][col]
 
+            table3 = sd3.add_table(rows=len(self.sNp_paths)+1, cols=2)
+            table3.style = 'TableGrid'
+            table3.cell(0, 1).text = "Trace Name"
+            table3.cell(0, 0).text = "sNp file"
+
+            for row in range(len(self.sNp_paths)):
+                table3.cell(row+1, 1).text = "TS"+str(row)
+                table3.cell(row+1, 0).text = self.ts_manager.file_titles[row]
+
             for i in range(self.count_plots()):
-                sd3.add_picture('img'+str(i)+'.png', width=Inches(6.5))
+                sd4.add_picture('img'+str(i)+'.png', width=Inches(6.5))
 
             self.d_info['DUTIdentification'] = sd1
             self.d_info['PortConfiguration'] = sd2
-            self.d_info['TestSummary'] = sd3
+            self.d_info['TestSummaryLegend'] = sd3
+            self.d_info['TestSummary'] = sd4
 
             self.tpl.render(self.d_info)
             print "rendered"
@@ -205,22 +217,23 @@ class Generator(object):
 
     def gen_plots(self):
         plot_count = self.count_plots()
-        s4p_count = len(self.s4p_paths)
+        sNp_count = len(self.sNp_paths)
         for p in range(plot_count):
             plt.clf()
             y_arr, labels = [], []
             self.plots = []
-            for t in range(s4p_count):
-                parameter = self.df_guide['s4p_'+str(t)].loc[2+p]
+            XY_list = []
+            for t in range(sNp_count):
+                parameter = self.df_guide['sNp_'+str(t)].loc[2+p]
                 magphase = self.df_guide['magphase'].loc[2+p]
                 title = self.df_guide['title'].loc[2+p]
                 if not(pd.isnull(parameter)): # if parameters is not blank
-                    labels.append("TS"+str(t))
-                    #self.ts_manager.db[0][1][0] ====> DB, TSfile 0, S21
+                    # self.ts_manager.db[0][1][0] ====> DB, TSfile 0, S21
                     split_list = parameter.split("_")
                     X = int("".join(ch for ch in split_list[0] if ch in "0123456789"))-1
                     Y = int("".join(ch for ch in split_list[1] if ch in "0123456789"))-1
-
+                    XY_list.append([X, Y])
+                    labels.append("TS"+str(t)+" S"+str(X+1)+str(Y+1))
                     x = np.array(self.ts_manager.ghz[t][X][Y])
                     if magphase == 'm':
                         y = np.array(self.ts_manager.db[t][X][Y])
@@ -228,9 +241,12 @@ class Generator(object):
                     else:
                         y = np.array(self.ts_manager.deg[t][X][Y])
                         plt.ylabel('Phase (deg)')
-                    plt.title('S'+str(X+1)+str(Y+1)+" "+title)
                     y_arr.append(y)
             plots_arr = []
+            if all(XY == XY_list[0] for XY in XY_list):
+                plt.title('S'+str(X+1)+str(Y+1))
+            else:
+                plt.title('Mixed Parameters')
             plt.xlabel('Frequency (GHz)')
             for ya in range(len(y_arr)):
                 plots_arr.append(plt.plot(x, y_arr[ya], label=labels[ya]))
@@ -253,6 +269,7 @@ class TouchstoneManager(object):
         self.pm = port_maps
         self.db, self.deg, self.ghz = [], [], []
         self.eng = matlab.engine.start_matlab()
+        self.file_titles = None
         for ts_p in range(len(ts_paths)):
             dbX_params = []
             degX_params = []
@@ -284,7 +301,32 @@ class TouchstoneManager(object):
             self.deg.append(degX_params)
             self.ghz.append(ghzX_params)
         self.eng.quit()
-        return
+        self.file_titles = self.get_sNp_titles(ts_paths)
+
+    def earliest_difference(self, list_of_strings, reverse = False):
+        '''
+        :param list_of_strings:
+        :return index: earliest point where strings are not the same
+        '''
+        min_length = len(min(list_of_strings, key=len))
+        for i in range(min_length):
+            if reverse == False:
+                ch = list_of_strings[0][i]
+            else:
+                ch = list_of_strings[0][-i-1]
+            for str in list_of_strings:
+                if reverse == False:
+                    if ch != str[i]:
+                        return i
+                else:
+                    if ch != str[-i-1]:
+                        return -i
+
+    def get_sNp_titles(self, string_list):
+        first = self.earliest_difference(string_list, reverse=False)
+        last = self.earliest_difference(string_list, reverse=True)
+        names = [x[first:last] for x in string_list]
+        return names
 
 
 def main():
@@ -297,8 +339,8 @@ def main():
         print "  i, info.csv filepath"
         print "  t, template.docx filepath"
         print "  g, guide.csv filepath"
-        print "  d, directory with .s4p files"
-        print "  p, figures to plot ie: 'f S11_m,S12_p,S22_m'"
+        print "  d, directory with .sNp files"
+        print "  p, figures to plot ie: 'f S1_1m,S1_2p,S2_2m'"
         print "     m is for magnitude, p is for phase"
         print
         print "USAGE EXAMPLE:"
@@ -311,7 +353,7 @@ def main():
             if ('t' in sys.argv and sys.argv.index('t') != len(sys.argv)-1) else os.getcwd()
         g.guide_path = sys.argv[sys.argv.index('g')+1] \
             if ('g' in sys.argv and sys.argv.index('g') != len(sys.argv)-1) else os.getcwd()
-        g.s4p_dir = sys.argv[sys.argv.index('d')+1] \
+        g.sNp_dir = sys.argv[sys.argv.index('d')+1] \
             if ('d' in sys.argv and sys.argv.index('d') != len(sys.argv)-1) else os.getcwd()
         g.figure_string = sys.argv[sys.argv.index('p')+1] \
             if ('p' in sys.argv and sys.argv.index('p') != len(sys.argv)-1) else "s1_1m,s1_1p,s2_1m,s2_1p"
